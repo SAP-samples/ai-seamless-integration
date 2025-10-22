20d1b7c49ed0edeb2424cf5d83bf3bce7caf72cd
Todor Stoyanov <t.stoyanov@sap.com>
2025-10-22 09:44:18 +0300

    Add busy indicator for each text generation (#8)
    * Add busy indicator for each text generation
    
    * change method name

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -44,8 +44,8 @@ export default class App extends BaseController {
 			outputValueState: ValueState.None,
 			outputEnabled: true,
 			buttonState: "generate",
-			sendButtonEnabled: true,
-			outputBusy: false
+			outputBusy: false,
+			sendButtonEnabled: true
 		} as ViewModelData;
 
 		const model = new JSONModel(initialModelData);
@@ -94,11 +94,8 @@ export default class App extends BaseController {
 				const keys = Object.keys(predefinedTexts[this.translationKey]);
 				const randomKey = keys[Math.floor(Math.random() * keys.length)];
 				this.currentTextKey = randomKey;
-				setTimeout(()=> {
-					this.generateText(predefinedTexts[this.translationKey][randomKey]);
-					this.viewModelData.outputBusy = false;
-					this.viewModel.updateBindings(true);
-				}, 1000)
+				this.viewModel.updateBindings(true);
+				this.startLoadingProcess(this.generateText, predefinedTexts[this.translationKey][randomKey] )
 				break;
 			case "generating":
 				this.viewModelData.buttonState = "revise";
@@ -226,50 +223,65 @@ export default class App extends BaseController {
 		const predefinedTextsSimplified = this.textObject.predefinedTextsSimplified;
 		const predefinedTextsSummarized = this.textObject.predefinedTextsSummarized;
 
+		this.viewModelData.outputBusy = true;
+		this.viewModel.updateBindings(true);
+
 		switch (event.getParameter("text")) {
 			case "Regenerate":
 				const keys = Object.keys(predefinedTexts[this.translationKey]);
 				const randomKey = keys[Math.floor(Math.random() * keys.length)];
 				this.currentTextKey = randomKey;
-				this.setStateAndGenerate("generating", randomKey, predefinedTexts);
+				this.startLoadingProcess(this.setStateAndGenerate, "generating", randomKey, predefinedTexts);
 				break;
 			case "Make Bulleted List":
-				this.startTextGeneration("reviseGenerating", predefinedTextsBulleted);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTextsBulleted);
 				break;
 			case "Clear Error":
+				this.viewModelData.outputBusy = false;
+				this.viewModel.updateBindings(true);
 				this.clearValueState();
 				break;
 			case "Fix Spelling and Grammar":
-				this.fixSpellingAndGrammar(predefinedTexts);
+				this.startLoadingProcess(this.fixSpellingAndGrammar, predefinedTexts);
 				break;
 			case "Generate Error":
+				this.viewModelData.outputBusy = false;
+				this.viewModel.updateBindings(true);
 				this.setNegativeValueState();
 				break;
 			case "Simplify":
-				this.startTextGeneration("reviseGenerating", predefinedTextsSimplified);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTextsSimplified);
 				break;
 			case "Expand":
-				this.startTextGeneration("reviseGenerating", predefinedTextsExpanded);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTextsExpanded);
 				break;
 			case "Rephrase":
-				this.startTextGeneration("reviseGenerating", predefinedTextsRephrased);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTextsRephrased);
 				break;
 			case "Summarize":
-				this.startTextGeneration("reviseGenerating", predefinedTextsSummarized);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTextsSummarized);
 				break;
 			case "Bulgarian":
 				this.translationKey = LanguageCode.BG;
-				this.startTextGeneration("reviseGenerating", predefinedTexts);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTexts);
 				break;
 			case "English":
 				this.translationKey = LanguageCode.EN;
-				this.startTextGeneration("reviseGenerating", predefinedTexts);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTexts);
 				break;
 			case "German":
 				this.translationKey = LanguageCode.DE;
-				this.startTextGeneration("reviseGenerating", predefinedTexts);
+				this.startLoadingProcess(this.startTextGeneration, "reviseGenerating", predefinedTexts);
 				break;
 		}
+	}
+
+	startLoadingProcess<TArgs extends unknown[]>(callback: (...args: TArgs)=> void, ...args: TArgs){
+		setTimeout(()=> {
+			callback.call(this, ...args);
+			this.viewModelData.outputBusy = false;
+			this.viewModel.updateBindings(true);
+		}, 1000);
 	}
 
 	sendTextHandler():void {
